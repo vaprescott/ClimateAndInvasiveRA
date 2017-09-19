cc094a697767568728bd903209be827166892c61
Prescott <vprescott@luc.edu>
2017-09-19 14:20:19 -0500

    added bias data file to code

--- ClimateRAcode.R
+++ ClimateRAcode.R
@@ -26,15 +26,15 @@ rcp45.50.list=list.files(path="E:/postdoc/Bioclim/Mod_WorldClim/Modlayers_2050_4
                          pattern="tif$", full.names=TRUE )
 rcp45.50=stack(rcp45.50.list)
 #RCP 45 2070
-rcp45.70.list=list.files(path="E:/postdoc/WorldClim/2070/GF-RCP45/gf45bi70/", 
+rcp45.70.list=list.files(path="E:/postdoc/Bioclim/Mod_WorldClim/Modlayers_2070_45/mod_70_45_tiff_gl", 
                          pattern="tif$", full.names=TRUE )
 rcp45.70=stack(rcp45.70.list)
 #RCP85 2050
-rcp85.50.list=list.files(path="E:/postdoc/Bioclim/Mod_WorldClim/Modlayers_2050_48/mod_50_85_tiff_gl", 
+rcp85.50.list=list.files(path="E:/postdoc/Bioclim/Mod_WorldClim/Modlayers_2050_85/mod_50_85_tiff_gl", 
                          pattern="tif$", full.names=TRUE )
 rcp85.50=stack(rcp85.50.list)
 #RCP85 2070
-rcp85.70.list=list.files(path="E:/postdoc/WorldClim/2070/GF-RCP45/gf45bi70/", 
+rcp85.70.list=list.files(path="E:/postdoc/Bioclim/Mod_WorldClim/Modlayers_2070_85/mod_70_85_tiff_gl", 
                          pattern="tif$", full.names=TRUE )
 rcp85.70=stack(rcp45.70.list)
 #great lakes current
@@ -63,12 +63,12 @@ pres_test=sp.coords[group==1,]
 
 #create background training and test data (in lieu of absence data)
 set.seed(10)
-max.x<-max(sp.coords$Longitude)
-min.x<-min(sp.coords$Longitude)
-max.y<-max(sp.coords$Latitude)
-min.y<-min(sp.coords$Latitude)
-ext=extent(min.x,max.x,min.y,max.y)
-backg=randomPoints(current, n=500,ext=ext, extf=1.25)
+ext=extent(bias3)
+bias3<-readOGR("E:/postdoc/analysis_files/background/misgu3_MCP/mis_poly_1.shp")
+backg=randomPoints(current, 
+                   n=nrow(sp.coords),
+                   ext=ext,
+                   extf=1.25)
 colnames(backg)=c('Longitude','Latitude')
 #backg<-backg[,c("Longitude","Latitude")]
 group=kfold(backg,5)
@@ -78,10 +78,10 @@ backg_test=backg[group==1,]
 #create a raster of rasterstack, showing just first layer and plot to check data
 r=raster(current,1)
 plot(!is.na(r), col=c('white','light grey'), legend=FALSE)
-points(backg_train, pch='-', cex=0.5, col='white')
+points(backg_train, pch='-', cex=0.5, col='blue')
 points(backg_test, pch='+', cex=0.5, col='black')
-points(pres_train, pch='+', col='green')
-points(pres_test, pch='+', col='purple')
+points(pres_train, pch='+', cex=0.5, col='green')
+points(pres_test, pch='+', cex=0.5,col='purple')
 
 #create data frame with presence and background training scores and environmental data
 backg_train_current<-extract(current, backg_train)
@@ -119,7 +119,7 @@ e<-evaluate(p=pres, a=abs)
 e
 threshold(e)
 tr<-threshold(e, "equal_sens_spec")
-form=sprintf('E:/postdoc/threshold/threshold_%s.csv',filename)
+form=sprintf('E:/postdoc/analysis_files/threshold/threshold_%s.csv',filename)
 write.csv(tr,
           file=form)
 sensitivity<-sum(pres>=tr)/length(pres) 
@@ -128,89 +128,105 @@ specificity<-sum(abs<tr)/length(abs)
 form1=sprintf('E:/postdoc/sensitivity/sensitivity_%s.csv',filename)
 write.csv(sensitivity,
           file=form1 )
-form2<-sprintf('E:/postdoc/specificity/specificity_%s.csv',filename)
+form2<-sprintf('E:/postdoc/analysis_files/specificity/specificity_%s.csv',filename)
 write.csv(specificity,
           file=form2)
-
+TSS<-sensitivity + specificity - 1
+form3<-sprintf('E:/postdoc/analysis_files/TSS/TSS_%s.csv', filename)
+write.csv(TSS,
+          file=form3)
 
 #run the model across the world (compare climate at gps points to the great lakes)
 #Takes about 5 hours to run
+#save a png of the plot
 current.predict<-predict(gl.current, sp.tc5.lr01.train,
            n.trees=sp.tc5.lr01.train$gbm.call$best.trees,
            type="response")
-
-#plot map
-colfun<-colorRampPalette(c("blue","cyan","green","yellow","red"))
-plot(current.predict, xlim=c(-95,-70), ylim=c(40,52),
-     col=colfun(50))
-plot(glb, add=TRUE)
-
-
-#to crop to just North America
-#cropped<-raster("E:/postdoc/Mod_WorldClim/ModLayers_2050_45/mod_rasters/mod_bio_1.tif")
-#current_NA<-crop(predict_current_mask, cropped)
-#current_NA<-mask(current_NA,cropped)
-#plot(current_NA,
-#     main="ADD TITLE")
-
+colfun<-colorRampPalette(
+  c("blue","cyan","green","yellow","red"))
+png(paste0("E:/postdoc/analysis_files/png_files/current/current_", filename, ".png"))
+current.plot<-levelplot(current.predict, 
+          main=paste(filename, "current"), 
+          xlim=c(-95,-70), ylim=c(40,52),
+          at=seq(0,1, length.out=1000),
+          col.regions=colfun,
+          margin=F,
+          maxpixels=13000000) +
+  layer(sp.polygons(glb))
+print(current.plot)
+dev.off()
 
 #Once again, the next step takes hours to run
 rcp45.50.predict<-predict(rcp45.50,sp.tc5.lr01.train,
             n.trees=sp.tc5.lr01.train$gbm.call$best.trees,
             type="response")
-#predict_RCP45_50_cropped<-crop(predict_RCP45_50,cropped)
-#predict_RCP45_50_mask<-mask(predict_RCP45_50_cropped,cropped)
-plot(rcp45.50.predict, 
-     main="P. henslowanum, RCP 4.5 2050", 
-     xlim=c(-95,-70), ylim=c(40,52),
-     col=colfun(50))
-plot(glb, add=TRUE)
-           
+png(paste0("E:/postdoc/analysis_files/png_files/4550/4550_", filename, ".png"))
+plot.4550<-levelplot(rcp45.50.predict, 
+          main=paste(filename, "RCP 4.5 2050"), 
+          xlim=c(-95,-70), ylim=c(40,52),
+          at=seq(0,1, length.out=1100),
+          col.regions=colfun,
+          margin=F,
+          maxpixels=15000000) +
+  layer(sp.polygons(glb))
+print(plot.4550)
+dev.off()
 
 rcp45.70.predict<-predict(rcp45.70, sp.tc5.lr01.train,
                          n.trees=sp.tc5.lr01.train$gbm.call$best.trees,
                          type="response")
-plot(rcp45.70.predict, 
-     main="P. henslowanum, RCP 4.5 2070", 
-     xlim=c(-95,-70), ylim=c(40,52),
-     col=colfun(50))
-plot(glb, add=TRUE)
-#rcp45.70_crop=crop(rcp45.7.predict,cropped)
-#rcp45.70_mask<-mask(rcp45.70_crop,cropped)
-#plot(rcp45.70_mask, xlim=c(-170,-20), ylim=c(10,90),
-#     main="A. torrentium RCP 4.5 2070", cex.main=1.25, 
-#     breaks=breakpoints,col=colors)
-#map(database="state", col="black",fill=FALSE, add=TRUE)
-#text(cex.main=1.25,add=TURE)
+png(paste0("E:/postdoc/analysis_files/png_files/4570/4570_", filename, ".png"))
+plot.4570<-levelplot(rcp45.70.predict, 
+          main=paste(filename, "RCP 4.5 2070"), 
+          xlim=c(-95,-70), ylim=c(40,52),
+          at=seq(0,1, length.out=1000),
+          col.regions=colfun,
+          margin=F,
+          maxpixels=15000000) +
+  layer(sp.polygons(glb))
+print(plot.4570)
+dev.off()
 
 
 rcp85.50.predict<-predict(rcp85.50, sp.tc5.lr01.train,
                          n.trees=sp.tc5.lr01.train$gbm.call$best.trees,
                          type="response")
-plot(rcp85.50.predict, 
-     main="P. henslowanum, RCP 8.5 2050", 
-     xlim=c(-95,-70), ylim=c(40,52),
-     col=colfun(50))
-plot(glb, add=TRUE)
+png(paste0("E:/postdoc/analysis_files/png_files/8550/8550_", filename, ".png"))
+plot.8550<-levelplot(rcp85.50.predict, 
+          main=paste(filename, "RCP 8.5 2050"), 
+          xlim=c(-95,-70), ylim=c(40,52),
+          at=seq(0,1, length.out=1100),
+          col.regions=colfun,
+          margin=F,
+          maxpixels=15000000) +
+  layer(sp.polygons(glb))
+print(plot.8550)
+dev.off()
 
 
 rcp85.70.predict<-predict(rcp85.70, sp.tc5.lr01.train,
                          n.trees=sp.tc5.lr01.train$gbm.call$best.trees,
                          type="response")
-plot(rcp85.70.predict, 
-     main="P. henslowanum, RCP 8.5 2070", 
-     xlim=c(-95,-70), ylim=c(40,52),
-     col=colfun(50))
-plot(glb, add=TRUE)
+png(paste0("E:/postdoc/analysis_files/png_files/8570/8570_", filename, ".png"))
+plot.8570<-levelplot(rcp85.70.predict, 
+          main=paste(filename, "RCP 8.5 2070"), 
+          xlim=c(-95,-70), ylim=c(40,52),
+          at=seq(0,1, length.out=1100),
+          col.regions=colfun,
+          margin=F,
+          maxpixels=15000000) +
+  layer(sp.polygons(glb))
+print(plot.8570)
+dev.off()
 
 #create animation of 2050 and 2070 
-test<-stack(current.predict,rcp45.50.predict, rcp85.50.predict, rcp85.70.predict)
-animation<-animate(test, pause=2, n=100, 
-                   xlim=c(-95,-70), 
-                   ylim=c(40,52),
-                   col=colfun(50),
-                   maxpixels=7000000)
-warnings()
+#test<-stack(current.predict,rcp45.50.predict, rcp85.50.predict, rcp85.70.predict)
+#animation<-animate(test, pause=2, n=100, 
+#                   xlim=c(-95,-70), 
+#                   ylim=c(40,52),
+#                   col=colors,
+#                   breaks=breakpoints,
+#                   maxpixels=7000000)
 
 }
 
---
+++ notes and trials.R
@@ -0,0 +1,68 @@
+species<-list.files(path="C:/Users/vprescott/Desktop/RAMP2/Full_coords",
+                    pattern="csv", full.names=TRUE)
+current<-raster("E:/postdoc/Bioclim/WorldClim/Current/tif_files/bio_1.tif")
+plot(current)
+
+for(i in 1:length(species)){
+  sp.coords<-read.csv(species[i], header=TRUE)
+  sp.coords<-read.csv("C:/Users/vprescott/Desktop/RAMP2/Full_coords/Silurus_glanis_full.csv",
+                      header=TRUE)
+  nrow=nrow(sp.coords)
+  
+  #filename<-species[i]
+  filename<-sub(pattern = "(.*)\\..*$", replacement = "\\1",
+                basename(species[i]))
+  filename<-sub(pattern="_", replacement =" ", 
+                basename(filename))
+  filename<-sapply(strsplit(filename, "_full"), "[[",1)
+  set.seed(0)
+  group=kfold(sp.coords, 5)
+  pres_train=sp.coords[group!=1,]
+  pres_test=sp.coords[group==1,]
+  set.seed(10)
+  max.x<-max(sp.coords$Latitude)
+  min.x<-min(sp.coords$Latitude)
+  max.y<-max(sp.coords$Longitude)
+  min.y<-min(sp.coords$Longitude)
+  ext=extent(min.x,max.x,min.y,max.y)
+  backg=randomPoints(current, n=nrow,ext=ext, extf=1.25)
+  colnames(backg)=c('Longitude','Latitude')
+  group=kfold(backg,5)
+  backg_train=backg[group!=1,]
+  backg_test=backg[group==1,]
+  #plot(!is.na(current), 
+  #     col=c('white','light grey'), 
+  #     legend=FALSE,
+  #     ext=ext)
+  plot(sp.coords, main=c(filename, "current3"))
+        
+  #plot(!is.na(current), col=c('white','light grey'), legend=FALSE)
+  #plot(ext, add=TRUE)
+  #points(sp.coords, pch=".", cex=0.5, col='red')
+  #points(backg_train, pch='-', cex=0.5, col='yellow')
+  #points(backg_test, pch='+', cex=0.5, col='black')
+  #points(pres_train, pch='+', col='green')
+  #points(pres_test, pch='+', col='blue') 
+}
+
+for(i in 1:length(species)){
+  sp.coords<-read.csv(species[i], header=TRUE)
+  filename<-sub(pattern = "(.*)\\..*$", replacement = "\\1",
+                basename(species[i]))
+  filename<-sub(pattern="_", replacement =" ", 
+                basename(filename))
+  filename<-sapply(strsplit(filename, "_full"), "[[",1)
+  plot(sp.coords, main=c(filename, "current"))
+}
+
+colfun<-colorRampPalette(
+  c("blue","cyan","green","yellow","red"))
+  
+levelplot(rcp45.50.predict, 
+          main=paste(filename, "RCP 4.5 2050"), 
+          xlim=c(-95,-70), ylim=c(40,52),
+          at=seq(0,1, length.out=120),
+          col.regions=colfun)
+          
+          
+          
