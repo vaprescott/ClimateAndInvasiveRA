300d3adc364c634c3eb1956f5f161d26421851a0
Prescott <vprescott@luc.edu>
2017-10-11 15:15:29 -0500

    created training and test data files for RAMP

--- RAMPtraintest.R
+++ RAMPtraintest.R
@@ -1,16 +1,31 @@
-#get training and test data for RAMP
-mt<-read.csv(file="C:/Users/vprescott/Desktop/RAMP2/Full_coords/Pisidiu_henslowanum_full.csv")
+library("plyr", lib.loc="~/R/win-library/3.4")
 
-#created unique points BEFORE saving
-#mt<- unique(mt)
+#get training and test data for RAMP
+species<-list.files(path="E:/postdoc/analysis_files/sp_coords/full_coords",
+                    pattern="csv", full.names=TRUE)
+for(i in 1:length(species)){
+  sp.coords<-read.csv(species[i], header=TRUE)
+filename<-sub(pattern = "(.*)\\..*$", replacement = "\\1",
+                basename(species[i]))
+filename<-sub(pattern="_", replacement =" ", 
+                basename(filename))
+filename<-sapply(strsplit(filename, "_full2"), "[[",1)  
+sp.coords<-sp.coords[,c("latitude","longitude")]
+rename(sp.coords, c("latitude"="Latitude","longitude"="Longitude"))
 
 #create training and test data
 set.seed(0)
-group=kfold(mt, 5)
-pres_train=mt[group!=1,]
-pres_test=mt[group==1,]
+group=kfold(sp.coords, 5)
+pres_train=sp.coords[group!=1,]
+pres_test=sp.coords[group==1,]
 
+formTrain<-sprintf('C:/Users/vprescott/Desktop/RAMP2/training/train_%s.csv', filename)
 write.csv(pres_train,
-          file="C:/Users/vprescott/Desktop/RAMP2/training/Pisidium_henslowanum.csv")
-write.csv(pres_test, 
-          file="C:/Users/vprescott/Desktop/RAMP2/test/Pisidium_henslowanum.csv")
+          file=formTrain,
+          row.names=FALSE)
+
+formTest<-sprintf('C:/Users/vprescott/Desktop/RAMP2/test/test_%s.csv', filename)
+write.csv(pres_test,
+          file=formTest,
+         row.names=FALSE)
+}
